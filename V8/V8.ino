2b58f21e43428b19bdb9aa71d21f9ec30022c00d
LeonardozBreunig <leonardozbreunig@gmail.com>
2016-05-11 08:08:26 -0300

    Pacote

--- V8/V8.ino
+++ V8/V8.ino
@@ -1,13 +1,11 @@
 #include "TimerOne.h"
 
-#define on 1023   //brilho do led em nivel alto
-#define off 0 //brilho do led em nivel baixo
 #define limite_luz 500 //valor entre 0 e 1023 para separar entre nivel alto e baixo
 #define tamanho_pacote 8 //tamanho do pacote
-#define tempo 5 //indica o tempo entre o chaveamento
-
+#define tempo 4 //indica o tempo em ms
+#define tempoUs 4000    //mesmo tempo em us
 int atual,anterior,count;
-int x[tamanho_pacote]={1,1,0,0,0,0,0,0};
+int x[tamanho_pacote]={1,0,1,0,1,0,1,0};
 
 
 
@@ -15,19 +13,23 @@ void recebePacote(){         //armazena em um vetor os valores recebidos na entr
   int pacote[tamanho_pacote];
   for(int i=0; i<tamanho_pacote; i++){
     pacote[i]=recebeBit();
-    Serial.println("Recebendo");
-    Serial.println(pacote[i]);;
     delay(tempo);
   }
+  Serial.println("Pacote Recebido");
+  for(int i=0; i<tamanho_pacote; i++){
+    Serial.print(pacote[i]);
+  }
+  Serial.println();
 }
 
 int recebeBit(){
   int dado;
   digitalWrite(2,HIGH);
   dado=analogRead(A0);
-  delay(tempo);
+  dado=dado+analogRead(A0);
+  dado=dado+analogRead(A0);
   digitalWrite(2,LOW);
-  ligaLed();
+  dado=floor(dado/3);
   if(dado>=limite_luz)
     dado=1;
   else
@@ -36,51 +38,41 @@ int recebeBit(){
 }
 
 void ligaLed(){
-  digitalWrite(3,on);
+  digitalWrite(3,HIGH);
 }
 
 void enviaBit(int dado){
     digitalWrite(3,x[dado]);
-    delay(tempo);
 }
 
 void transmite(){
-  digitalWrite(3,off);
-  delay(tempo);
+  digitalWrite(3,LOW);
 }
 
 void enviaPacote(){
   noInterrupts();
   count++;
   interrupts();
-  if(count==1){
+  if(count==(99-tamanho_pacote)){
     transmite();
-    geraPacote();
   }
-  if((count>=2)&&(count<10)){
-    enviaBit(count-2);
+  if((count>=(100-tamanho_pacote))&&(count<100)){
+    enviaBit(count-(100-tamanho_pacote));
   }
-  if(count==1000){
+  if(count==100){
    noInterrupts();
    count=0;
    interrupts();
+   ligaLed();
 }
-}
-
-void geraPacote(){
-  for(int i=0; i<tamanho_pacote;i++){
-    if(x[i]==1)
-    x[i]=on;
-    else
-    x[i]=off;
-  }
 }
 
 void setup() {
   Serial.begin(9600);
   pinMode(2,OUTPUT);
   pinMode(3,OUTPUT);
-  Timer1.initialize(100);
+  Timer1.initialize(10);
+  Timer1.setPeriod(tempoUs);
   Timer1.attachInterrupt(enviaPacote);
   ligaLed();
 }
@@ -90,6 +82,7 @@ void loop() {
   atual=recebeBit();
   interrupts();
   if(atual==0 && anterior==1){       //queda, incio de recepção 
+    delay(tempo);
     recebePacote();
     noInterrupts();
     anterior=atual;
